104379b2720dcc202aad1836eb4798269221b438
Liza Tinku <lizatj2017@example.com>
2025-07-13 23:23:03 +0000

    Update redeploy-site.sh for systemd deployment

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,15 +1,7 @@
 #!/bin/bash
 
-# Kill all tmux sessions
-tmux kill-server
-
-cd ~/pe-portfolio || exit 1
-
+cd /root/pe-portfolio
 git fetch && git reset origin/main --hard
-
 source python3-virtualenv/bin/activate
-
 pip install -r requirements.txt
-
-# Start a new detached tmux session running the Flask server
-tmux new-session -d -s flask "cd ~/pe-portfolio && source python3-virtualenv/bin/activate && export FLASK_ENV=development && flask run --host=0.0.0.0"
+systemctl restart myportfolio.service
