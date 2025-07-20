096440b93b50630e9472afb1f44065b57e17caf1
ethanvillalovoz <ethan.villalovoz@gmail.com>
2025-07-19 18:11:32 -0700

    Implemented test app file, tests pass

---
+++ tests/test_app.py
@@ -0,0 +1,34 @@
+# tests/test_app.py
+
+import unittest
+import os
+os.environ['TESTING'] = 'true'
+
+from app import app, TimelinePost
+
+class AppTestCase(unittest.TestCase):
+    def setUp(self):
+        self.client = app.test_client()
+        # Create tables before each test
+        TimelinePost.create_table()
+
+    def tearDown(self):
+        # Drop tables after each test to keep the DB clean
+        TimelinePost.drop_table()
+
+    def test_home(self):
+        response = self.client.get("/")
+        assert response.status_code == 200
+        html = response.get_data(as_text=True)
+        assert "<title>MLH Fellow</title>" in html
+        # TODO Add more tests relating to the home page
+
+    def test_timeline(self):
+        response = self.client.get("/api/timeline_post")
+        assert response.status_code == 200
+        assert response.is_json
+        json = response.get_json()
+        assert "timeline_posts" in json
+        assert len(json["timeline_posts"]) == 0
+        # TODO Add more tests relating to the /api/timeline_post GET and POST apis
+        # TODO Add more tests relating
