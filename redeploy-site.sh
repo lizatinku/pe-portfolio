36cddf4f2634af5adb60bfb9bb79ac3e531ff39a
Liza Tinku Jose <ltinkujose@ucdavis.edu>
2025-07-28 23:11:11 -0700

    Added script for Docker-based deployment"

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
-
 cd /root/pe-portfolio
+
+echo "Fetching latest changes..."
 git fetch && git reset origin/main --hard
-source python3-virtualenv/bin/activate
-pip install -r requirements.txt
-systemctl restart myportfolio.service
+
+echo "Stopping containers..."
+docker compose -f docker-compose.prod.yml down
+
+echo "Rebuilding and starting containers..."
+docker compose -f docker-compose.prod.yml up -d --build
+
+echo "Deployment complete!"
